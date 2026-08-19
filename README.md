45414ca6a4aa07b7d16f24168b65daadea4db41a
Lucas da Silva Carvalho <156864774+LucasBlazzer@users.noreply.github.com>
2026-08-19 20:41:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api-produtos-2
+Na Prática - exercício de fixação de conteúdo
---
+++ src/main/java/com/lucas/api_produtos/ApiProdutosApplication.java
@@ -0,0 +1,17 @@
+package com.lucas.api_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(
+				ApiProdutosApplication.class,
+				args
+		);
+	}
+
+}
